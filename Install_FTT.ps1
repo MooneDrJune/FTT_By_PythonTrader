c02ce2a2ee013ceb1d3a7aa893f8842ee2fb1762
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2023-01-24 00:22:19 +0530

    Add files via upload

--- Install_FTT.ps1
+++ Install_FTT.ps1
@@ -4,16 +4,17 @@ mkdir $destinationpath
 $url = 'https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-64bit-build1.zip'
 $tmp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } -PassThru
 Invoke-WebRequest -OutFile $tmp $url
-Microsoft.PowerShell.Archive\Expand-Archive -Path $tmp -DestinationPath $destinationpath -Force -PassThru
+Microsoft.PowerShell.Archive\Expand-Archive -Path $tmp -DestinationPath $destinationpath -Force
 $tmp | Remove-Item
 
 cd $ariafolder
 
 $url = 'https://github.com/MooneDrJune/FTT_By_PythonTrader/releases/download/V.0.3/FinvasiaTradeTerminal.zip'
-.\aria2c.exe  --out="FinvasiaTradeTerminal.zip" $url
-Microsoft.PowerShell.Archive\Expand-Archive -Path .\FinvasiaTradeTerminal.zip -DestinationPath ..\ -Force -PassThru
+$aria2cdownload = '& $ariafolder\aria2c.exe --out FinvasiaTradeTerminal.zip $url'
+Invoke-Expression $aria2cdownload
+Microsoft.PowerShell.Archive\Expand-Archive -Path "$ariafolder\FinvasiaTradeTerminal.zip" -DestinationPath $destinationpath -Force
 cd $destinationpath
-Remove-Item .\aria2-1.36.0-win-64bit-build1\ -Force -Recurse
+Remove-Item $ariafolder -Force -Recurse
 
 write-output "Creating FinvasiaTradeTerminal Icon on Desktop"
 
@@ -29,6 +30,3 @@ $Shortcut.WorkingDirectory = "$env:USERPROFILE\FinvasiaTradeTerminal"
 $shortcut.WindowStyle = 1
 $ShortCut.Hotkey = "CTRL+ALT+SHIFT+F"
 $shortcut.Save()
-
-$desktopfolder = "$env:USERPROFILE\Desktop"
-cd $desktopfolder
